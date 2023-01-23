669cc49dbd664b7d61d53a9ae19904dbd48190a0
Ivy <0617ivytang@gmail.com>
2023-01-23 00:07:38 -0500

    update 4

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ function initialize()
 {
     diceTable = document.getElementById("dicetable");
     diceToRoll = document.getElementById("diceToRoll")
-    /*rollDice();
-    button = document.createElement("button");
-    button.innerHTML = "Roll Dice";
-    button.addEventListener("click", rollDice);
-    document.body.appendChild(button);
-    inputContainer = document.getElementById("inputform");
-    DiceToRollOutput = document.getElementById("diceToRoll"); */
     TimesToRollDiceOutput = document.getElementById("timesRoll");
 }
 
@@ -17,6 +10,13 @@ function rollDice()
     let totals = []; // empty array to store totals
     diceToRoll = parseInt(document.getElementById("diceToRoll").value); // takes in integer value from form which determines if 1, 2, or 3 dice is rolled
     // console.log(diceToRoll); check
+    timesRoll = parseInt(document.getElementById("timesRoll").value); // takes in integer value for how many times the dice are rolled
+    // console.log(timesRoll); check
+    result = [];
+    for (let i = 0; i < timesRoll; i++)
+    {
+        result.push(roll(diceToRoll)); // push appends into array
+    }
 
     if(diceToRoll == 1) // 1 die = totals can only be 1-6
     {
@@ -25,14 +25,13 @@ function rollDice()
     else if(diceToRoll == 2) // 2 dice = totals can only be 2-12
     {
         totals = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        doubles(result);
     }
     else // 3 dice = totals can only be 3-18
     {
         totals = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+        triples(result);
     }
-
-    timesRoll = parseInt(document.getElementById("timesRoll").value); // takes in integer value for how many times the dice are rolled
-    // console.log(timesRoll); check
 
     let allSums = []; // empty array for all the totals of all the times rolled
     for (let i = 0; i < timesRoll; i++)
@@ -60,37 +59,51 @@ function rollDice()
         }
     }
 
-    // console.log(mode);
-    // console.log(frequencies);
-    // console.log(mean(allSums));
-    /*
-    var numRolls = getRandomInteger(25, 100);
+    dicetable = document.getElementById("dicetable"); // get dicetable from HTML
     
-    for (var i = 0; i < numRolls; i++)
+    for (let i = 0; i < timesRoll; i++)
     {
-        var dieRoll = getRandomInteger(1, 6);
-
+        dieRoll = result[i];
         var newRow = diceTable.insertRow();
         var newCell = newRow.insertCell();
-        newCell.innerHTML = "Roll " + (i + 1);
-
-        newCell = newRow.insertCell();
         newCell.innerHTML = dieRoll;
-    }
-    
-    DiceToRoll = inputContainer.dicenum.value;
-    TimesToRollDice = inputContainer.timesroll.value;
+        newCell = newRow.insertCell();
+        newCell.innerHTML = largestFrequency;
+        newCell = newRow.insertCell();
+        newCell.innerHTML = mean(result);
+        newCell = newRow.insertCell();
+        newCell.innerHTML = median(result);
+        newCell = newRow.insertCell();
+        newCell.innerHTML = mode;
+        if(diceToRoll >= 2)
+        {
+            newCell = newRow.insertCell();
+            newCell.innerHTML = doubles(result);
+        }
+        else
+        {
+            newCell = newRow.insertCell();
+            newCell.innerHTML = 0;
+        }
 
-    inputContainer.dicenum.value = "";
-    inputContainer.timesroll.value = "";
-    display(); */
+        if(diceToRoll == 3)
+        {
+            newCell = newRow.insertCell();
+            newCell.innerHTML = triples(result);
+        }
+        else
+        {
+            newCell = newRow.insertCell();
+            newCell.innerHTML = 0;
+        }
+    }
 }
 
 function frequency(result, numsRolled) // checks how many times one result has appeared 
 {
     count = 0;
-    // console.log(result);
-    // console.log(numsRolled);
+    // console.log(result); check
+    // console.log(numsRolled); check
     for (let num of numsRolled)
     {
         if (num == result)
@@ -124,7 +137,7 @@ function mean(numsRolled)
 function median(sums)
 {
     return sums.sort()[Math.floor(sums.length / 2)]; // first sort through the array so it's lowest to highest value
-    // Math.floor is if the number is odd because you have to remove the decimal since there are only whole numbers on the dice
+    // Math.floor is if the number is odd because you have to divide the number in half
 }
 
 function doubles(rolls) 
@@ -153,24 +166,9 @@ function triples(rolls) // checks from each roll with THREE dice
     return count; // tells the user how many times ALL 3 dice rolled the same result
 }
 
-
-
 function getRandomInteger(lower, upper) // like Math.random()
 {
     var multiplier = upper - (lower - 1);
     var rnd = parseInt(Math.random() * multiplier) + lower;
     return rnd;
 }
-
-function display()
-{
-    numberOutput.innerHTML = number;
-    diceToRoll.innerHTML = getRandomInteger(1, 3);
-}
-/* random = [3, 8, 3];
-sum = 0;
-for(i of random)
-{
-    sum += i;
-}
-console.log(sum/random.length); */
